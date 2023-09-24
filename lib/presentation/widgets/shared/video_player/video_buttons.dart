f6f2e7b10b0908a42d97676c70450988ca3f43cb
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-24 18:37:04 -0500

    add the animated buttom with fernando herrera package

--- lib/presentation/widgets/shared/video_player/video_buttons.dart
+++ lib/presentation/widgets/shared/video_player/video_buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:tiktok_alike/config/helpers/human_formats.dart';
 
 import '../../../../domain/entities/video_post.dart';
@@ -15,8 +16,19 @@ class VideoButtons extends StatelessWidget {
             value: video.likes,
             iconData: Icons.favorite,
             iconColor: Colors.red),
+        const SizedBox(
+          height: 20,
+        ),
         _CustomIconButton(
-            value: video.views, iconData: Icons.remove_red_eye_outlined)
+            value: video.views, iconData: Icons.remove_red_eye_outlined),
+        const SizedBox(
+          height: 20,
+        ),
+        SpinPerfect(
+            infinite: true,
+            duration: const Duration(seconds: 5),
+            child: const _CustomIconButton(
+                value: 0, iconData: Icons.play_circle_outline))
       ],
     );
   }
@@ -43,7 +55,7 @@ class _CustomIconButton extends StatelessWidget {
               color: color,
               size: 30,
             )),
-        Text(HumanFormats.humanReadbleNumber(value.toDouble()))
+        if (value > 0) Text(HumanFormats.humanReadbleNumber(value.toDouble()))
       ],
     );
   }
