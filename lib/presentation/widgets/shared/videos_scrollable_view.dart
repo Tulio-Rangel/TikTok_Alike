331067f1093fd352a86509129e67868b93bc9048
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-24 18:03:32 -0500

    create like & views buttoms

--- lib/presentation/widgets/shared/videos_scrollable_view.dart
+++ lib/presentation/widgets/shared/videos_scrollable_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_alike/presentation/widgets/shared/video_player/video_buttons.dart';
 
 import '../../../domain/entities/video_post.dart';
 
@@ -9,17 +10,24 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical, // Cambia el scroll a vertical
       physics:
-          const BouncingScrollPhysics(), // fisica de rebote cuando se llega al final de las paginas
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.green),
-        Container(color: Colors.orange),
-        Container(color: Colors.deepPurple)
-      ],
+          const BouncingScrollPhysics(), // fisica de rebote cuando se llega al final de las paginas, se debe poner para que funcione en android, en apple lo tiene por defecto
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+
+        return Stack(
+          children: [
+            // Video Player + gradiente
+
+            // Botones
+            Positioned(
+                bottom: 40, right: 20, child: VideoButtons(video: videoPost))
+          ],
+        );
+      },
     );
   }
 }
