540f85048018a35d92a5b5843ce7ff8b78648e43
Tulio-Rangel <tjrn20@hotmail.com>
2023-09-26 18:51:38 -0500

    add backgroound gradient

--- lib/presentation/widgets/video/fullscreen_player.dart
+++ lib/presentation/widgets/video/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_alike/presentation/widgets/video/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -61,6 +62,9 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
               VideoPlayer(controller),
 
               // Gradiente
+              VideoBackGround(
+                stops: const [0.8, 1.0],
+              ),
 
               // Texto del video
               Positioned(
